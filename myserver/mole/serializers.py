4f0301de6f056806ac6d1fbc17784d1b06efe0ea
Flint444 <maksimgrabauskas@gmail.com>
2022-10-14 22:05:07 +0200

    2st version #Очень сильно упрощён код #Добавлены роутеры и ModelViewSet

--- myserver/mole/serializers.py
+++ myserver/mole/serializers.py
@@ -1,33 +1,9 @@
-import io
-
 from rest_framework import serializers
-from rest_framework.parsers import JSONParser
-from rest_framework.renderers import JSONRenderer
 
 from .models import Women
-
-#class WomenModel:
-#    def __init__(self, title, content):
-#        self.title = title
-#        self.content = content
 
 class WomenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Women
-        #fields = ("title","content","cat")
         fields = "__all__"
 
-
-#def encode():
-#    model = WomenModel('Angelina Jolie', 'Content: Angelina Jolie')
-#    model_sr = WomenSerializer(model)
-#    print(model_sr.data, type(model_sr.data), sep = '\n')
-#    json = JSONRenderer().render(model_sr.data)
-#    print(json)
-
-#def decode():
-#    stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"Content: Angelina Jolie"}')
-#    data = JSONParser().parse(stream)
-#    serializers = WomenSerializer(data=data)
-#    serializers.is_valid()
-#    print(serializers.validated_data)
